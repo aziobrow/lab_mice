9351372db4827215de4c183400204c6aea75e808
Aurora Ziobrowski <aziobrow@gmail.com>
2018-01-06 20:15:53 -0700

    fixed route namespacing for tests, one test for routing needs redoing

--- config/routes.rb
+++ config/routes.rb
@@ -8,14 +8,14 @@ Rails.application.routes.draw do
         get '/mice/all' => :index, as: 'mice'
         get '/mouse/:original_id' => :show, as: 'mouse'
       end
+      namespace :mice do
+        controller :search do
+          get '/find' => :index
+        end
+      end
     end
   end
 
-  namespace :mice do
-    controller :search do
-      get '/find' => :index
-    end
-  end
 
   controller :sessions do
     get '/login' => :new
