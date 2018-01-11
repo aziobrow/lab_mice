68001cb4af5360fac22dca4de0f1ef0bd975c0f1
Aurora Ziobrowski <aziobrow@gmail.com>
2018-01-10 19:28:24 -0700

    added some styling for filtered views

--- app/controllers/filter_controller.rb
+++ app/controllers/filter_controller.rb
@@ -1,6 +1,7 @@
 class FilterController < ApplicationController
   def index
     @mice = MousePresenter.new(query_params).mice
+    @filters = query_params.values
   end
 
 private
--- app/presenters/mouse_data_presenter.rb
+++ app/presenters/mouse_data_presenter.rb
@@ -6,44 +6,57 @@ attr_reader :mouse
     @mouse = mouse
   end
 
+  def mouse_is_favorite?(current_user)
+    return true if @mouse.users.include?(current_user)
+    false
+  end
+
+  def display_original_id
+    "Mouse ##{mouse.original_id}"
+  end
+
   def display_status
-    @mouse.status.titleize
+    mouse.status.titleize
+  end
+
+  def display_group_number
+    mouse.group_number
   end
 
   def display_diet
-    @mouse.diet.titleize
+    mouse.diet.titleize
   end
 
   def display_color
-    @mouse.color.titleize
+    mouse.color.titleize
   end
 
   def display_sex
-    @mouse.sex.titleize
+    mouse.sex.titleize
   end
 
   def display_trisomic
-    @mouse.trisomic.to_s.titleize
+    mouse.trisomic.to_s.titleize
   end
 
   def display_protein
-    if @mouse.protein_ug_per_ml?
-      return @mouse.protein_ug_per_ml
+    if mouse.protein_ug_per_ml?
+      return mouse.protein_ug_per_ml
     else
       return "N/A"
     end
   end
 
   def display_weight
-    if @mouse.weight_in_grams?
-      return @mouse.weight_in_grams
+    if mouse.weight_in_grams?
+      return mouse.weight_in_grams
     else
       return "N/A"
     end
   end
 
   def format_updated_timestamp
-    time = @mouse.updated_at.localtime
+    time = mouse.updated_at.localtime
     time.strftime("%m/%d/%Y, %I:%M%P")
   end
 end
