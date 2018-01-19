26ba4b8d68066f4c4a285119d768b3b79c341aaa
Aurora Ziobrowski <aziobrow@gmail.com>
2018-01-18 20:36:26 -0700

    footer rendering correctly

--- app/assets/javascripts/dropdown.js
+++ app/assets/javascripts/dropdown.js
@@ -1,40 +1,43 @@
-$( function() {
-  $('#protocolMenuBtn').click(toggleMiceDropdown)
-})
+$(document).on('turbolinks:load', function()  {
 
-$( function() {
-  $('#profileMenuBtn').click(toggleProfileDropdown)
-})
+  $( function() {
+    $('#protocolMenuBtn').click(toggleMiceDropdown)
+  })
 
-$( function() {
-  $('#notificationBtn').click(toggleNotificationDropdown)
-})
+  $( function() {
+    $('#profileMenuBtn').click(toggleProfileDropdown)
+  })
 
-function toggleMiceDropdown() {
-  var displayStatus = $('#protocolMenu').css('display');
-  if (displayStatus === 'block')  {
-    $("#protocolMenu").css("display", "none");
-  } else {
-    $("#protocolMenu").css("display", "block");
+  $( function() {
+    $('#notificationBtn').click(toggleNotificationDropdown)
+  })
+
+  function toggleMiceDropdown() {
+    var displayStatus = $('#protocolMenu').css('display');
+    if (displayStatus === 'block')  {
+      $("#protocolMenu").css("display", "none");
+    } else {
+      $("#protocolMenu").css("display", "block");
+    }
   }
-}
 
-function toggleProfileDropdown() {
-  var displayStatus = $('#profileMenu').css('display');
-  if (displayStatus === 'block')  {
-    $("#profileMenu").css("display", "none");
-  } else {
-    console.log(displayStatus)
-    $("#profileMenu").css("display", "block");
+  function toggleProfileDropdown() {
+    var displayStatus = $('#profileMenu').css('display');
+    if (displayStatus === 'block')  {
+      $("#profileMenu").css("display", "none");
+    } else {
+      console.log(displayStatus)
+      $("#profileMenu").css("display", "block");
+    }
   }
-}
 
-function toggleNotificationDropdown() {
-  var displayStatus = $('#notificationMenu').css('display');
-  if (displayStatus === 'block')  {
-    $("#notificationMenu").css("display", "none");
-  } else {
-    console.log(displayStatus)
-    $("#notificationMenu").css("display", "block");
+  function toggleNotificationDropdown() {
+    var displayStatus = $('#notificationMenu').css('display');
+    if (displayStatus === 'block')  {
+      $("#notificationMenu").css("display", "none");
+    } else {
+      console.log(displayStatus)
+      $("#notificationMenu").css("display", "block");
+    }
   }
-}
+})
