61ca3cd046c10238c300cf4d87bbecb91210bf99
Aurora Ziobrowski <aziobrow@gmail.com>
2018-01-18 23:04:49 -0700

    tabs for notes completed styled

--- app/assets/javascripts/tabs.js
+++ app/assets/javascripts/tabs.js
@@ -1,18 +1,30 @@
 $(document).on('turbolinks:load', function()  {
 
+var lastContentId;
+var lastButtonId;
+
   $( function() {
     var contentId = $('.tabcontent').attr('id')
     $('.tablinks').click(toggleTabContent)
   })
 
   function toggleTabContent(event) {
-    var tabId = event.target.id;
-    var tabContentId = `#${tabId}Content`;
-    var displayStatus = $(tabContentId).css('display');
-    if (displayStatus === 'block')  {
-      $(tabContentId).css("display", "none");
-    } else {
-      $(tabContentId).css("display", "block");
+    var tabButtonId = event.target.id;
+    var tabContentId = `#${tabButtonId}Content`;
+    if (tabContentId != lastContentId) {
+      $(lastContentId).css("display", "none");
+      if (lastButtonId != null) {
+        $(`#${lastButtonId}`).removeClass("active");
+      }
+      lastButtonId = tabButtonId;
+      lastContentId = tabContentId;
     }
+    var displayStatus = $(tabContentId).css('display');
+      if (displayStatus === 'block')  {
+        $(tabContentId).css("display", "none");
+      } else {
+        $(tabContentId).css("display", "block");
+        $(`#${tabButtonId}`).addClass("active");
+      }
   }
 })
