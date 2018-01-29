13b5f2c46af9ea146354436b8d08d90209f07e9f
Aurora Ziobrowski <aziobrow@gmail.com>
2018-01-28 21:10:23 -0700

    dropdowns fixed to only show one at a time

--- app/assets/javascripts/dropdown.js
+++ app/assets/javascripts/dropdown.js
@@ -1,5 +1,7 @@
 $(document).on('turbolinks:load', function()  {
 
+var lastSelectionId;
+
   $( function() {
     $('#protocolMenuBtn').click(toggleMiceDropdown)
   })
@@ -18,40 +20,53 @@ $(document).on('turbolinks:load', function()  {
 
   function toggleMiceDropdown() {
     var displayStatus = $('#protocolMenu').css('display');
+    if (lastSelectionId != '#protocolMenu') {
+      $(lastSelectionId).css("display", "none");
+    }
     if (displayStatus === 'block')  {
       $("#protocolMenu").css("display", "none");
     } else {
       $("#protocolMenu").css("display", "block");
     }
+    lastSelectionId = '#protocolMenu'
   }
 
   function toggleProfileDropdown() {
     var displayStatus = $('#profileMenu').css('display');
+    if (lastSelectionId != '#profileMenu') {
+      $(lastSelectionId).css("display", "none");
+    }
     if (displayStatus === 'block')  {
       $("#profileMenu").css("display", "none");
     } else {
-      console.log(displayStatus)
       $("#profileMenu").css("display", "block");
     }
+    lastSelectionId = '#profileMenu'
   }
 
   function toggleNotificationDropdown() {
     var displayStatus = $('#notificationMenu').css('display');
+    if (lastSelectionId != '#notificationMenu') {
+      $(lastSelectionId).css("display", "none");
+    }
     if (displayStatus === 'block')  {
       $("#notificationMenu").css("display", "none");
     } else {
-      console.log(displayStatus)
       $("#notificationMenu").css("display", "block");
     }
+    lastSelectionId = '#notificationMenu'
   }
 
   function toggleManagementDropdown() {
     var displayStatus = $('#manageMenu').css('display');
+    if (lastSelectionId != '#manageMenu') {
+      $(lastSelectionId).css("display", "none");
+    }
     if (displayStatus === 'block')  {
       $("#manageMenu").css("display", "none");
     } else {
-      console.log(displayStatus)
       $("#manageMenu").css("display", "block");
     }
+    lastSelectionId = '#manageMenu'
   }
 })
